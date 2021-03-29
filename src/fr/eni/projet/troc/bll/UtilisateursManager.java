2e7454f78908b91fb0dfee81c08f6a11d2293382
Jean-Baptiste Ginguene <jbginguene@gmail.com>
2021-03-29 17:19:37 +0200

    ajout packages util et package exception + MAJ UtilisateurManager

--- src/fr/eni/projet/troc/bll/UtilisateursManager.java
+++ src/fr/eni/projet/troc/bll/UtilisateursManager.java
@@ -6,6 +6,8 @@ package fr.eni.projet.troc.bll;
 import fr.eni.projet.troc.bo.Utilisateur;
 import fr.eni.projet.troc.dal.DAOFactory;
 import fr.eni.projet.troc.dal.UtilisateurDAO;
+import fr.eni.projet.troc.exception.BusinessException;
+import fr.eni.projet.troc.util.Constants;
 
 /**
  * Classe en charge
@@ -33,4 +35,30 @@ public class UtilisateursManager {
 		utilisateurDAO.create(utilisateur);
 	}
 
+	private boolean validerTelephone(String telephone, BusinessException be) {
+		if (telephone == null) {
+			be.addError("Le numéro de téléphone est obligatoire");
+			return false;
+		}
+		if (!telephone.matches(Constants.PATTERN_TEL)) {
+			be.addError(
+					"Mot de passe doit contenir entre 8 et 12 caractères (1 chiffre, 1 majuscule, 1 caractère spécial)");
+			return false;
+		}
+		return true;
+	}
+
+	private boolean validerPassword(String pwd, BusinessException be) {
+		if (pwd == null) {
+			be.addError("Mot de passe est obligatoire");
+			return false;
+		}
+		if (!pwd.matches(Constants.PATTERN_PWD)) {
+			be.addError(
+					"Mot de passe doit contenir entre 8 et 12 caractères (1 chiffre, 1 majuscule, 1 caractère spécial)");
+			return false;
+		}
+
+		return true;
+	}
 }
---
+++ src/fr/eni/projet/troc/util/Constants.java
@@ -0,0 +1,6 @@
+package fr.eni.projet.troc.util;
+
+public interface Constants {
+	String PATTERN_TEL = "^(?:(?:\\+|00)33[\\s.-]{0,3}(?:\\(0\\)[\\s.-]{0,3})?|0)[1-9](?:(?:[\\s.-]?\\d{2}){4}|\\d{2}(?:[\\s.-]?\\d{3}){2})$\r\n";
+
+}
