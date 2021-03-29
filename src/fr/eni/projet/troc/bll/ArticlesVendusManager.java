f584ad1dee1e3e3f75f9e630fa5c7ea6bac42650
Julliot <bjulliot35@gmail.com>
2021-03-29 14:55:05 +0200

    En cours de création de laffichage de la page accueil

---
+++ src/fr/eni/projet/troc/bll/ArticlesVendusManager.java
@@ -0,0 +1,29 @@
+package fr.eni.projet.troc.bll;
+
+import java.util.List;
+
+import fr.eni.projet.troc.bo.ArticleVendu;
+import fr.eni.projet.troc.dal.ArticleVenduDAO;
+import fr.eni.projet.troc.dal.DAOFactory;
+
+public class ArticlesVendusManager {
+
+	private ArticleVenduDAO articleVenduDAO;
+	private static ArticlesVendusManager instance;
+
+	private ArticlesVendusManager() {
+		articleVenduDAO = DAOFactory.getArticleVenduDAO();
+	}
+
+	public static ArticlesVendusManager getInstance() {
+		if (instance == null) {
+			instance = new ArticlesVendusManager();
+		}
+		return instance;
+	}
+	
+    public List<ArticleVendu> getAllArticleVendus() throws Exception {
+        return articleVenduDAO.selectAll();
+    }
+	
+}
