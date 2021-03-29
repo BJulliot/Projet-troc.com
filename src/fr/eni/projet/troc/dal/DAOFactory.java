b5ff2095d9f013c2231acf6e882e367cc83ce45f
Jean-Baptiste Ginguene <jbginguene@gmail.com>
2021-03-29 14:40:19 +0200

    creation UtilisateurDAO, UtilisateurImpl, et MAJ DAOFactory

--- src/fr/eni/projet/troc/dal/DAOFactory.java
+++ src/fr/eni/projet/troc/dal/DAOFactory.java
@@ -3,10 +3,9 @@
  */
 package fr.eni.projet.troc.dal;
 
-
-
 /**
  * Classe en charge
+ * 
  * @author Bastien
  * @version Troc.com - v1.0
  * @date 29 mars 2021 - 14:11:51
@@ -16,5 +15,9 @@ public class DAOFactory {
 	public static ArticleVenduDAO getArticleVenduDAO() {
 		return new ArticleVenduImpl();
 	}
-	
+
+	public static UtilisateurDAO getUtilisateurDAO() {
+		return new UtilisateurImpl();
+	}
+
 }
