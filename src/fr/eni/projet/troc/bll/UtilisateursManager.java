63749c7c9851a9daf0ccc225b5aaf8d5929ddf18
Julliot <bjulliot35@gmail.com>
2021-03-29 11:37:31 +0200

    Ajout classe de base dans les packages

---
+++ src/fr/eni/projet/troc/bll/UtilisateursManager.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package fr.eni.projet.troc.bll;
+
+/**
+ * Classe en charge
+ * @author Bastien
+ * @version Troc.com - v1.0
+ * @date 29 mars 2021 - 11:36:34
+ */
+public class UtilisateursManager {
+
+}
---
+++ src/fr/eni/projet/troc/bo/Utilisateur.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package fr.eni.projet.troc.bo;
+
+/**
+ * Classe en charge
+ * @author Bastien
+ * @version Troc.com - v1.0
+ * @date 29 mars 2021 - 11:36:47
+ */
+public class Utilisateur {
+
+}
