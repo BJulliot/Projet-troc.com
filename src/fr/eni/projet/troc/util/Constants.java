5bae1f4b542e5b4d89875494d81cb3fb10cdac59
Jean-Baptiste Ginguene <jbginguene@gmail.com>
2021-03-31 10:38:41 +0200

    modif regex password

--- src/fr/eni/projet/troc/util/Constants.java
+++ src/fr/eni/projet/troc/util/Constants.java
@@ -2,6 +2,6 @@ package fr.eni.projet.troc.util;
 
 public interface Constants {
 	String PATTERN_TEL = "^(\\+33|0|0033)[0-9]{9}$";
-	String PATTERN_PWD = "^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$";
+	String PATTERN_PWD = "^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+.;,=]).*$";
 	String PATTERN_EMAIL = "^(.+)@(.+)$";
 }
